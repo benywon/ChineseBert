748af7ab69add6eddcf5a032093a7058a15e6c6c
benywon <bingning.wang@nlpr.ia.ac.cn>
2019-08-08 19:35:00 +0800

    Add files via upload

---
+++ get_model.sh
@@ -0,0 +1,6 @@
+wget -P resource https://drive.google.com/open?id=1H1b4G8a4plnZp762kmU4Sflo1OQ0kqVc
+wget -P resource https://drive.google.com/open?id=1dhXJoN8pHYG1ZLRxx405VcHxXpFKH0kc
+tar xvf model.512.8.th.tgz
+tar xvf model.1024.16.th.tgz
+
+
